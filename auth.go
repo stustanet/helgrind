fad76306999ddbff74fed7c256496aea4d193b06
Julien Schmidt <js@stusta.net>
2018-04-14 15:09:55 +0200

    add systemd socket activation support

--- auth.go
+++ auth.go
@@ -9,18 +9,27 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/sha256"
-	"crypto/tls"
 	"crypto/x509"
 	"encoding/hex"
 	"encoding/pem"
 	"errors"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
-	"time"
 )
+
+func loadCaCert(filepath string) (*x509.CertPool, error) {
+	caCertBytes, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		return nil, err
+	}
+	caCertPool := x509.NewCertPool()
+	if ok := caCertPool.AppendCertsFromPEM(caCertBytes); !ok {
+		return nil, errors.New("could not add CA cert")
+	}
+	return caCertPool, nil
+}
 
 func loadPrivKey(filepath string) (key *rsa.PrivateKey, err error) {
 	bytes, err := ioutil.ReadFile(filepath)
@@ -55,28 +64,14 @@ func loadPrivKey(filepath string) (key *rsa.PrivateKey, err error) {
 	return nil, errors.New("unsupported private key type")
 }
 
-func main() {
-	var cfg config
-	err := cfg.parseFile("etc/helgrind.json")
+func signFuncFromPrivKey(filepath string) (func(m string) string, error) {
+	privKey, err := loadPrivKey(filepath)
 	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(cfg)
-
-	caCertBytes, err := ioutil.ReadFile(cfg.CaCert)
-	if err != nil {
-		log.Fatal(err)
-	}
-	caCertPool := x509.NewCertPool()
-	caCertPool.AppendCertsFromPEM(caCertBytes)
-
-	privKey, err := loadPrivKey(cfg.ServerPrivKey)
-	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	// sign := func(m string) string {
-	// 	sig, _ := rsa.SignPKCS1v15(rand.Reader, privKey, 0, []byte(m))
-	// 	return hex.EncodeToString(sig)
+	//  sig, _ := rsa.SignPKCS1v15(rand.Reader, privKey, 0, []byte(m))
+	//  return hex.EncodeToString(sig)
 	// }
 	rng := rand.Reader
 	sign := func(m string) string {
@@ -84,49 +79,7 @@ func main() {
 		sig, _ := rsa.SignPKCS1v15(rng, privKey, crypto.SHA256, hashed[:])
 		return hex.EncodeToString(sig)
 	}
-
-	// http server config
-	server := &http.Server{
-		Addr: cfg.Listen,
-		Handler: &authHandler{
-			Services: cfg.Services,
-			Sign:     sign,
-		},
-		TLSConfig: &tls.Config{
-			// security settings
-			MinVersion:               tls.VersionTLS12,
-			PreferServerCipherSuites: true,
-			CipherSuites: []uint16{
-				// Mozilla recommended ciphers (modern)
-				// https://wiki.mozilla.org/Security/Server_Side_TLS
-				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
-				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-				// ECDHE-ECDSA-AES256-SHA384
-				// ECDHE-RSA-AES256-SHA384
-				tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
-				tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
-			},
-			CurvePreferences: []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
-
-			// client certs
-			ClientAuth: tls.RequireAndVerifyClientCert,
-			ClientCAs:  caCertPool,
-		},
-
-		// timeouts
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		IdleTimeout:  120 * time.Second,
-	}
-
-	// start serving HTTPS
-	if err = server.ListenAndServeTLS(cfg.ServerCertChain, cfg.ServerPrivKey); err != nil {
-		log.Fatal(err)
-	}
+	return sign, nil
 }
 
 func sendHTTPErr(w http.ResponseWriter, code int) {
---
+++ helgrind.go
@@ -0,0 +1,94 @@
+// Copyright 2018 Julien Schmidt. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be found
+// in the LICENSE file.
+
+package main
+
+import (
+	"crypto/tls"
+	"log"
+	"net/http"
+	"time"
+
+	"gitlab.stusta.de/stustanet/helgrind/internal/systemd"
+)
+
+func main() {
+	var cfg config
+	err := cfg.parseFile("etc/helgrind.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	caCertPool, err := loadCaCert(cfg.CaCert)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sign, err := signFuncFromPrivKey(cfg.ServerPrivKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// http server config
+	server := &http.Server{
+		Handler: &authHandler{
+			Services: cfg.Services,
+			Sign:     sign,
+		},
+		TLSConfig: &tls.Config{
+			// security settings
+			MinVersion:               tls.VersionTLS12,
+			PreferServerCipherSuites: true,
+			CipherSuites: []uint16{
+				// Mozilla recommended ciphers (modern)
+				// https://wiki.mozilla.org/Security/Server_Side_TLS
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				// ECDHE-ECDSA-AES256-SHA384
+				// ECDHE-RSA-AES256-SHA384
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
+			},
+			CurvePreferences: []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+
+			// client certs
+			ClientAuth: tls.RequireAndVerifyClientCert,
+			ClientCAs:  caCertPool,
+		},
+
+		// timeouts
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
+	// start serving HTTPS
+	if cfg.Listen == "systemd" {
+		sockets, err := systemd.Listen()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if len(sockets) != 1 {
+			log.Fatalf("unexpected number of systemd sockets: expected 1, got %d\n", len(sockets))
+		}
+		listener, err := sockets[0].Listener()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if err = server.ServeTLS(listener, cfg.ServerCertChain, cfg.ServerPrivKey); err != nil {
+			log.Fatal(err)
+		}
+
+	} else {
+		server.Addr = cfg.Listen
+		if err = server.ListenAndServeTLS(cfg.ServerCertChain, cfg.ServerPrivKey); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
